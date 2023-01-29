23fc71c3c8423f6b99d3d55a23b712a09b2b363d
kishek2000 <kishek12@gmail.com>
2023-01-29 15:51:11 +1100

    feat: completed 10x dataset reading

--- packages/backend/plumber.R
+++ packages/backend/plumber.R
@@ -4,11 +4,39 @@
 #* Base Endpoint
 #* @get /
 function() {
-    list(msg=paste('🚀🚀 AutoCAR Platform Analytics - Base Endpoint.'));
+    list(msg=paste('🚀🚀 AutoCAR Platform Analytics looking healthy!!'));
 }
 
 #* Health Check
 #* @get /health
 function() {
     list(msg=paste('🚀🚀 AutoCAR Platform Analytics looking healthy!!'));
+}
+
+#* Text File Test
+#* @param f:file
+#* @post /upload-echo
+function(f) {
+    content <- f[[1]];
+    list(msg=paste(content))
+}
+
+#* 10X Dataset
+#* @param files:[file] datasetName:string
+#* @post /upload-10X
+function(files, datasetName) {
+    print(datasetName)
+    dir_name = paste(datasetName, '-data', sep="");
+    dir.create(dir_name, showWarnings = FALSE);
+    idx = 1;
+    file_names = names(files);
+    file_path_prefix = paste(dir_name, "/", sep="");
+    for (file in files)
+    {
+        file_content <- file[[1]];
+        file_name <- file_names[idx];
+        file_path = paste(file_path_prefix, str_trim(file_name), sep="");
+        write(file_content, file = file_path);
+        idx = idx + 1;
+    }
 }
--- packages/backend/runPlumber.R
+++ packages/backend/runPlumber.R
@@ -1,9 +1,25 @@
 is_plumber_available <- require("plumber");
+is_sourcetools_available <- require("sourcetools");
+is_seurat_available <- require("Seurat");
+is_tidyverse_available <- require("tidyverse");
+is_dplyr_available <- require("dplyr");
+is_patchwork_available <- require("patchwork");
 
 if (!is_plumber_available) {
     install.packages("plumber", repos = "http://cran.us.r-project.org");
+    install.packages("sourcetools", repos = "http://cran.us.r-project.org");
+    install.packages("Seurat", repos = "http://cran.us.r-project.org");
+    install.packages("tidyverse", repos = "http://cran.us.r-project.org");
+    install.packages("dplyr", repos = "http://cran.us.r-project.org");
+    install.packages("patchwork", repos = "http://cran.us.r-project.org");
 }
 
 library("plumber");
+library("sourcetools");
+library("Seurat");
+library("tidyverse");
+library("dplyr");
+library("patchwork");
+
 pr("plumber.R") %>%
     pr_run(port = 8000)
